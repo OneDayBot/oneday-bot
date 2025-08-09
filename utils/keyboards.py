f0e4df7ed8b78ca60c256b4625ea19b7e15109d0
OneDayBot <onedaybot10@gmail.com>
2025-08-09 08:55:38 +0200

    Delete utils directory

--- utils/keyboards.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def city_keyboard():
-    """Клавіатура для вибору міста"""
-    buttons = [
-        [InlineKeyboardButton("Bratislava", callback_data="city_bratislava")],
-        [InlineKeyboardButton("Košice", callback_data="city_kosice")]
-    ]
-    return InlineKeyboardMarkup(buttons)
