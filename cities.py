d8de30102fe6e2fea6ae091565f1a19c0a916d16
OneDayBot <onedaybot10@gmail.com>
2025-08-09 07:58:11 +0200

    Update cities.py

--- cities.py
+++ cities.py
@@ -1,1 +1,12 @@
-
+CITY_CHANNELS = {
+    "Bratislava": "@OneDayBratislava",
+    "Košice": "@OneDayKosice",
+    "Prešov": "@OneDayPresov",
+    "Žilina": "@OneDayZilina",
+    "Nitra": "@OneDayNitra",
+    "Banská Bystrica": "@OneDayBanskaBystrica",
+    "Trnava": "@OneDayTrnava",
+    "Trenčín": "@OneDayTrencin",
+    "Martin": "@OneDayMartin",
+    "Poprad": "@OneDayPoprad"
+}
