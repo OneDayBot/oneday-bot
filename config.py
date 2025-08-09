f86320dee1571bf7dbf91bbedc5bf4c4ce12f9a6
OneDayBot <onedaybot10@gmail.com>
2025-08-09 08:59:24 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-from os import getenv
-
-class Config:
-    TOKEN = getenv("TELEGRAM_BOT_TOKEN")
-    ADMIN_ID = int(getenv("ADMIN_ID", 924805332))
-    MAIN_CHANNEL = getenv("MAIN_CHANNEL", "@OneDaySK")
-    SPAM_KEYWORDS = ["spam", "scam", "реклама"]
